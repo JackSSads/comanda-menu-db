69300e49c618302b6d54cd99b436a9bf81bf2da1
Jackson Souza <67111463+JackSSads@users.noreply.github.com>
2025-06-01 10:39:25 -0300

    Update category.sql

--- category.sql
+++ category.sql
@@ -1,12 +1,7 @@
 CREATE TABLE IF NOT EXISTS comanda_menu.category (
 	category_id		INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name_category	VARCHAR(50) NOT NULL,
-    screen			VARCHAR(20) NOT NULL,
-    created_at		DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updated_at		DATETIME ON UPDATE CURRENT_TIMESTAMP
-)
-
-SELECT * FROM comanda_menu.category;
-
-INSERT INTO comanda_menu.category (name_category, screen)
-VALUES ('Bebidas', 'bar')
+    	name_category		VARCHAR(50) NOT NULL,
+    	screen			VARCHAR(20) NOT NULL,
+    	created_at		DATETIME DEFAULT CURRENT_TIMESTAMP,
+    	updated_at		DATETIME ON UPDATE CURRENT_TIMESTAMP
+);
