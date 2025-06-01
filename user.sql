b152ce1d3219974187b37d2a131137636a70f945
Jackson Souza <67111463+JackSSads@users.noreply.github.com>
2025-06-01 10:41:28 -0300

    Update user.sql

--- user.sql
+++ user.sql
@@ -1,4 +1,3 @@
--- Usuários - OK
 CREATE TABLE IF NOT EXISTS `user` (
 	user_id		INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     username	VARCHAR(100) NOT NULL,
@@ -8,7 +7,3 @@ CREATE TABLE IF NOT EXISTS `user` (
     created_at	DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at	DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
-
-INSERT INTO `user` (username, email, password, func)
-VALUES ('ADM', 'example@test.com', '123', 'garcom');
-SELECT * FROM `user`;
