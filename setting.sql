e1b8d37682e72a034e342c0821b63073febcce58
Jackson Souza <67111463+JackSSads@users.noreply.github.com>
2025-07-01 19:53:16 -0300

    Update setting.sql

--- setting.sql
+++ setting.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS comanda_menu.setting (
 	service_change_percentage	FLOAT DEFAULT 0,
 	image_pix			MEDIUMBLOB DEFAULT NULL,
 	color				VARCHAR(30) DEFAULT NULL,
+	service_change_printer		BOOLEAN DEFAULT 0,
+	printer_name				VARCHAR(30) DEFAULT NULL,
 	created_at			DATETIME DEFAULT CURRENT_TIMESTAMP,
 	updated_at			DATETIME ON UPDATE CURRENT_TIMESTAMP
 );
